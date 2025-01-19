d810889b74bea82178253509b480d06776ef33ba
Niko Bertrand <nb@ni-be.dev>
2025-01-19 14:38:39 +0100

    some more cleanup

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
-# Implementing the Observe Think Act loop using Clingo and Clinguin. 
+# Implementing the Observe Think Act loop using Clinguin. 
 
-Based on the WUMPUS games as a working example. 
+In this repository you find the first iteration of the Observe Think Act Loop implented using Clinguin
+
+
+## Installation Instructions
+
+Clingo
+
+Clinguin
+
+Clingraph
+
+## Repository Structure
+
+- Backend
+  Location of the Backend Files
+
+- Ui Files
+  - multi_shot_ui.lp 
+  - single_shot_ui.lp
+
+- Multi_shot
+  - Agent Encoding for:
+    - m (Mapping)
+    - mp (Mapping + PickUp)
+    - mo (Mapping + Obstacle Avoidance)
+    - mpoi (Mapping + PickUp + Obstacle Avoidance and Interaction)
+    - agent_init_loc.lp **Domain file describing the agent initial location
+
+- Single_shot
+  - Agent Encoding for:
+    - m (Mapping)
+    - mp (Mapping + PickUp)
+    - mo (Mapping + Obstacle Avoidance)
+    - mpoi (Mapping + PickUp + Obstacle Avoidance and Interaction)
+
+- Environment
+  - TODO
+
+- Benchmarks
+
+- Instance Files
+  - Files for:
+    - m (Mapping)
+    - mp (Mapping + PickUp)
+    - mo (Mapping + Obstacle Avoidance)
+    - mpoi (Mapping + PickUp + Obstacle Avoidance and Interaction)
+
+## How to Run 
+
+### Single shot
+
+### Multi Shot
+'''Shell
+clinguin client-server --domain-file multi_shot/{**DOMAIN**_multi_shot_agent.lp, agent_init_loc.lp } --ui-files=ui_files/multi_shot_ui.lp --custom-classes backend/multi_shot_OTA_backend.py --backend multi_shot_backend --env-file multi_shot/environment_multi.lp --instance instances/**INSERT INSTANCE**.lp
+'''
+
+So that would be or example:
+
+TODO 
+'''Shell
+clinguin client-server --domain-file multi_shot/{agent_multi.lp, agent_init_loc.lp } --ui-files=ui_files/multi_shot_ui.lp --custom-classes backend/multi_shot_OTA_backend.py --backend ota_backend --env-file multi_shot/environment_multi.lp --instance instances/instance_simple.lp
+'''
+
+## License
--- backend/multi_shot_OTA_backend.py
+++ backend/multi_shot_OTA_backend.py
@@ -3,17 +3,14 @@ Custom Clinguin Backend for the Single Shot OTA implementation
 
 """
 
-import clingo
-from functools import cached_property
 import time
-from clinguin.server.application.backends.clingo_backend import UIState
+from functools import cached_property
 from clinguin.utils.annotations import extends
 from clinguin.server.application.backends import ClingoBackend
-from clinguin.server.data.domain_state import solve, tag
 from clingo import Control
 
 
-class ota_backend(ClingoBackend):
+class multi_shot_ota_backend(ClingoBackend):
     """
     First init command line, include arrays to store temp data in for computation
     """
@@ -70,14 +67,7 @@ class ota_backend(ClingoBackend):
                 self._env_data.append(atom)
 
     def _compute_env(self, step, action):
-        """
-        1. create control object for the environent
-        2. load the env. information and instance data
-        3. if actions have been chosen, in the UI/agent, include add them to ctl.
-        4. add the current time,
-        5. ground
-        6. solve
-        """
+        """ """
         ctl_env = Control()
         ctl_env.load(self._env_info[0])
         ctl_env.load(self._instance_info[0])
@@ -91,28 +81,20 @@ class ota_backend(ClingoBackend):
         ctl_env.solve(on_model=self.env_model)
 
     def agent_action_multi(self, step, action):
-        """
-        when action is triggered in UI, append action to the list of actions.
-        all actions that influence the env. are in here.
-
-        """
-
-        agent_start_time = time.perf_counter()
+        """ """
+        event_start_time = time.perf_counter()
         self._set_constant("x", f"{step}")
-        # self._outdate()
-        # self._init_ctl()
-        # self._ground("step", [step])
         self.add_assumption(action, "true")
 
         self._ground("step", [f"{int(step) + 1}"])
         self.set_external(f"query({int(step)})", "false")
 
         self.set_external(f"query({int(step)+1})", "true")
+
         agent_time = time.perf_counter()
-
-        agent_runtime = agent_time - agent_start_time
-
+        agent_runtime = agent_time - event_start_time
         env_start_time = time.perf_counter()
+
         self._compute_env(f"{int(step)}", action)
 
         env_time = time.perf_counter()
@@ -120,7 +102,7 @@ class ota_backend(ClingoBackend):
 
         self.update()
         runtime = time.perf_counter()
-        full_runtime = runtime - agent_start_time
+        full_runtime = runtime - event_start_time
 
         print(f"Step:{step}, AGENT RUNTIME: {agent_runtime}")
         print(f"Step:{step}, ENV RUNTIME: {env_runtime}")
